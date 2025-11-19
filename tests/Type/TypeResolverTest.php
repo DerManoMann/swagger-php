2750e333dde36f1b267e5f7f5e57dd8cebd737ae
DerManoMann <mano@radebatz.net>
2025-11-19 17:12:57 +1300

    Run test for both 3.0.0 and 3.1.0

--- tests/Type/TypeResolverTest.php
+++ tests/Type/TypeResolverTest.php
@@ -17,77 +17,115 @@ use OpenApi\Tests\Fixtures\PHP\DocblockAndTypehintTypes;
 use OpenApi\Tests\OpenApiTestCase;
 use OpenApi\TypeResolverInterface;
 
+use const PHP_VERSION_ID;
+
 class TypeResolverTest extends OpenApiTestCase
 {
     public static function resolverAugmentCases(): iterable
     {
-        if (\PHP_VERSION_ID < 80100) {
+        if (PHP_VERSION_ID < 80100) {
             return [];
         }
 
         $expectations = [
-            'nothing' => '{ "property": "nothing" }',
-            'string' => '{ "type": "string", "property": "string" }',
-            'nullablestring' => '{ "type": "string", "nullable": true, "property": "nullableString" }',
-            'nullablestringexplicit' => '{ "type": "string", "nullable": false, "property": "nullableStringExplicit" }',
-            'nullablestringdocblock' => '{ "type": "string", "nullable": true, "property": "nullableStringDocblock" }',
-            'nullablestringnative' => '{ "type": "string", "nullable": true, "property": "nullableStringNative" }',
-            'stringarray' => '{ "type": "array", "items": { "type": "string" }, "property": "stringArray" }',
-            'stringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "stringList" }',
-            'stringlistexplicit' => '{ "type": "array", "items": { "type": "string", "example": "foo" }, "property": "stringListExplicit" }',
-            'nullablestringlist' => '{ "type": "array", "items": { "type": "string" }, "nullable": true, "property": "nullableStringList" }',
-            'nullablestringlistunion' => '{ "type": "array", "items": { "type": "string" }, "nullable": true, "property": "nullableStringListUnion" }',
-            'class' => '{ "$ref": "#/components/schemas/DocblockAndTypehintTypes" }',
-            'nullableclass' => '{ "oneOf": [ { "$ref": "#/components/schemas/DocblockAndTypehintTypes" } ], "nullable": true, "property": "nullableClass" }',
-            'namespacedglobalclass' => '{ "type": "string", "format": "date-time", "property": "namespacedGlobalClass" }',
-            'nullablenamespacedglobalclass' => '{ "type": "string", "format": "date-time", "nullable": true, "property": "nullableNamespacedGlobalClass" }',
-            'alsonullablenamespacedglobalclass' => '{ "type": "string", "format": "date-time", "nullable": true, "property": "alsoNullableNamespacedGlobalClass" }',
-            'intrange' => '{ "type": "integer", "maximum": 10, "minimum": -9223372036854775808, "property": "intRange" }',
-            'positiveint' => '{ "type": "integer", "maximum": 9223372036854775807, "minimum": 1, "property": "positiveInt" }',
-            'nonzeroint' => '{ "type": "integer", "not": { "enum": [ 0 ] }, "property": "nonZeroInt" }',
-            'arrayshape' => '{ "type": "array", "items": { "type": "boolean" }, "property": "arrayShape" }',
-            'legacy:uniontype' => '{ "property": "unionType" }',
-            'type-info:uniontype' => '{ "type": [ "integer", "string" ], "property": "unionType" }',
-            'promotedstring' => '{ "type": "string", "property": "promotedString" }',
-            'mixedunion' => '{ "example": "My value", "property": "mixedUnion" }',
-            'getstring' => '{ "type": "string", "property": "getString" }',
-            'paramdatetimelist' => '{ "type": "array", "items": { "type": "string", "format": "date-time" }, "property": "paramDateTimeList" }',
-            'paramstringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "paramStringList" }',
-            'blah' => '{ "type": "string", "example": "My blah", "nullable": true, "property": "blah" }',
-            'blah_values' => '{ "type": "array", "items": { "type": "string", "example": "hello" }, "nullable": true, "property": "blah_values" }',
-            'oneofvar' => '{ "oneOf": [ { "type": "string" }, { "type": "bool" } ], "property": "oneOfVar" }',
-            'oneoflist' => '{ "type": "array", "items": { "oneOf": [ { "type": "string" }, { "type": "bool" } ] }, "property": "oneOfList" }',
+            OA\OpenApi::VERSION_3_0_0 => [
+                'nothing' => '{ "property": "nothing" }',
+                'string' => '{ "type": "string", "property": "string" }',
+                'nullablestring' => '{ "type": "string", "nullable": true, "property": "nullableString" }',
+                'nullablestringexplicit' => '{ "type": "string", "nullable": false, "property": "nullableStringExplicit" }',
+                'nullablestringdocblock' => '{ "type": "string", "nullable": true, "property": "nullableStringDocblock" }',
+                'nullablestringnative' => '{ "type": "string", "nullable": true, "property": "nullableStringNative" }',
+                'stringarray' => '{ "type": "array", "items": { "type": "string" }, "property": "stringArray" }',
+                'stringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "stringList" }',
+                'stringlistexplicit' => '{ "type": "array", "items": { "type": "string", "example": "foo" }, "property": "stringListExplicit" }',
+                'nullablestringlist' => '{ "type": "array", "items": { "type": "string" }, "nullable": true, "property": "nullableStringList" }',
+                'nullablestringlistunion' => '{ "type": "array", "items": { "type": "string" }, "nullable": true, "property": "nullableStringListUnion" }',
+                'class' => '{ "$ref": "#/components/schemas/DocblockAndTypehintTypes" }',
+                'nullableclass' => '{ "oneOf": [ { "$ref": "#/components/schemas/DocblockAndTypehintTypes" } ], "nullable": true, "property": "nullableClass" }',
+                'namespacedglobalclass' => '{ "type": "string", "format": "date-time", "property": "namespacedGlobalClass" }',
+                'nullablenamespacedglobalclass' => '{ "type": "string", "format": "date-time", "nullable": true, "property": "nullableNamespacedGlobalClass" }',
+                'alsonullablenamespacedglobalclass' => '{ "type": "string", "format": "date-time", "nullable": true, "property": "alsoNullableNamespacedGlobalClass" }',
+                'intrange' => '{ "type": "integer", "maximum": 10, "minimum": -9223372036854775808, "property": "intRange" }',
+                'positiveint' => '{ "type": "integer", "maximum": 9223372036854775807, "minimum": 1, "property": "positiveInt" }',
+                'nonzeroint' => '{ "type": "integer", "not": { "enum": [ 0 ] }, "property": "nonZeroInt" }',
+                'arrayshape' => '{ "type": "array", "items": { "type": "boolean" }, "property": "arrayShape" }',
+                'uniontype' => '{ "property": "unionType" }',
+                'promotedstring' => '{ "type": "string", "property": "promotedString" }',
+                'mixedunion' => '{ "example": "My value", "property": "mixedUnion" }',
+                'getstring' => '{ "type": "string", "property": "getString" }',
+                'paramdatetimelist' => '{ "type": "array", "items": { "type": "string", "format": "date-time" }, "property": "paramDateTimeList" }',
+                'paramstringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "paramStringList" }',
+                'blah' => '{ "type": "string", "example": "My blah", "nullable": true, "property": "blah" }',
+                'blah_values' => '{ "type": "array", "items": { "type": "string", "example": "hello" }, "nullable": true, "property": "blah_values" }',
+                'oneofvar' => '{ "oneOf": [ { "type": "string" }, { "type": "bool" } ], "property": "oneOfVar" }',
+                'oneoflist' => '{ "type": "array", "items": { "oneOf": [ { "type": "string" }, { "type": "bool" } ] }, "property": "oneOfList" }',
+            ],
+            OA\OpenApi::VERSION_3_1_0 => [
+                'nothing' => '{ "property": "nothing" }',
+                'string' => '{ "type": "string", "property": "string" }',
+                'nullablestring' => '{ "type": [ "string", "null" ], "property": "nullableString" }',
+                'nullablestringexplicit' => '{ "type": "string", "property": "nullableStringExplicit" }',
+                'nullablestringdocblock' => '{ "type": [ "string", "null" ], "property": "nullableStringDocblock" }',
+                'nullablestringnative' => '{ "type": [ "string", "null" ], "property": "nullableStringNative" }',
+                'stringarray' => '{ "type": "array", "items": { "type": "string" }, "property": "stringArray" }',
+                'stringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "stringList" }',
+                'stringlistexplicit' => '{ "type": "array", "items": { "type": "string", "example": "foo" }, "property": "stringListExplicit" }',
+                'nullablestringlist' => '{ "type": [ "array", "null" ], "items": { "type": "string" }, "property": "nullableStringList" }',
+                'nullablestringlistunion' => '{ "type": [ "array", "null" ], "items": { "type": "string" }, "property": "nullableStringListUnion" }',
+                'class' => '{ "$ref": "#/components/schemas/DocblockAndTypehintTypes" }',
+                'nullableclass' => '{ "oneOf": [ { "$ref": "#/components/schemas/DocblockAndTypehintTypes" }, { "type": "null" } ], "property": "nullableClass" }',
+                'namespacedglobalclass' => '{ "type": "string", "format": "date-time", "property": "namespacedGlobalClass" }',
+                'nullablenamespacedglobalclass' => '{ "type": [ "string", "null" ], "format": "date-time", "property": "nullableNamespacedGlobalClass" }',
+                'alsonullablenamespacedglobalclass' => '{ "type": [ "string", "null" ], "format": "date-time", "property": "alsoNullableNamespacedGlobalClass" }',
+                'intrange' => '{ "type": "integer", "maximum": 10, "minimum": -9223372036854775808, "property": "intRange" }',
+                'positiveint' => '{ "type": "integer", "maximum": 9223372036854775807, "minimum": 1, "property": "positiveInt" }',
+                'nonzeroint' => '{ "type": "integer", "not": { "const": 0 }, "property": "nonZeroInt" } ',
+                'arrayshape' => '{ "type": "array", "items": { "type": "boolean" }, "property": "arrayShape" }',
+                'legacy:uniontype' => '{ "property": "unionType" }',
+                'type-info:uniontype' => '{ "type": [ "integer", "string" ], "property": "unionType" }',
+                'promotedstring' => '{ "type": "string", "property": "promotedString" }',
+                'mixedunion' => '{ "example": "My value", "property": "mixedUnion" }',
+                'getstring' => '{ "type": "string", "property": "getString" }',
+                'paramdatetimelist' => '{ "type": "array", "items": { "type": "string", "format": "date-time" }, "property": "paramDateTimeList" }',
+                'paramstringlist' => '{ "type": "array", "items": { "type": "string" }, "property": "paramStringList" }',
+                'blah' => '{ "type": [ "string", "null" ], "example": "My blah", "property": "blah" }',
+                'blah_values' => '{ "type": [ "array", "null" ], "items": { "type": "string", "example": "hello" }, "property": "blah_values" }',
+                'oneofvar' => '{ "oneOf": [ { "type": "string" }, { "type": "bool" } ], "property": "oneOfVar" }',
+                'oneoflist' => '{ "type": "array", "items": { "oneOf": [ { "type": "string" }, { "type": "bool" } ] }, "property": "oneOfList" }',
+            ],
         ];
 
         $rc = new \ReflectionClass(DocblockAndTypehintTypes::class);
 
         foreach (static::getTypeResolvers() as $key => $typeResolver) {
-            $analysis = (new Generator())
-                ->setVersion(OA\OpenApi::VERSION_3_1_0)
-                ->setProcessorPipeline(new Pipeline([new MergeIntoOpenApi(), new AugmentSchemas()]))
-                ->withContext(function (Generator $generator, Analysis $analysis, Context $context) use ($rc) {
-                    $generator->generate([$rc->getFileName()], $analysis, false);
+            foreach ([OA\OpenApi::VERSION_3_0_0, OA\OpenApi::VERSION_3_1_0] as $version) {
+                $analysis = (new Generator())
+                    ->setVersion($version)
+                    ->setProcessorPipeline(new Pipeline([new MergeIntoOpenApi(), new AugmentSchemas()]))
+                    ->withContext(function (Generator $generator, Analysis $analysis, Context $context) use ($rc) {
+                        $generator->generate([$rc->getFileName()], $analysis, false);
 
-                    return $analysis;
-                });
+                        return $analysis;
+                    });
 
-            $schema = $analysis->getAnnotationForSource(DocblockAndTypehintTypes::class);
+                $schema = $analysis->getAnnotationForSource(DocblockAndTypehintTypes::class);
 
-            foreach ($schema->properties as $ii => $property) {
-                $property->property = $property->_context->property
-                    // promoted properties might not have a name!
-                    ?? $property->_context->method;
+                foreach ($schema->properties as $ii => $property) {
+                    $property->property = $property->_context->property
+                        // promoted properties might not have a name!
+                        ?? $property->_context->method;
 
-                $caseName = strtolower($property->property);
-                $resolverCaseName = "$key:$caseName";
-                $fullCase = "$key-[$ii]-$caseName";
+                    $caseName = strtolower($property->property);
+                    $resolverCaseName = "$key:$caseName";
+                    $fullCase = "$key:{$version}[$ii]-$caseName";
 
-                yield $fullCase => [
-                    $typeResolver,
-                    $analysis,
-                    $property,
-                    json_decode($expectations[$caseName] ?? $expectations[$resolverCaseName] ?? '{}', true),
-                ];
+                    yield $fullCase => [
+                        $typeResolver,
+                        $analysis,
+                        $property,
+                        json_decode($expectations[$version][$caseName] ?? $expectations[$version][$resolverCaseName] ?? '{}', true),
+                    ];
+                }
             }
         }
     }
