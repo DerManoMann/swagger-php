e2b6abc96b8c6cbecd8a372aa5c96a7a9d2463d1
DerManoMann <mano@radebatz.net>
2026-07-24 21:55:14 +1200

    WIP: add spec snippets and update test

--- tests/DocSnippetsTest.php
+++ tests/DocSnippetsTest.php
@@ -7,8 +7,11 @@
 namespace OpenApi\Tests;
 
 use OpenApi\Attributes\OpenApi;
+use OpenApi\Builder;
+use OpenApi\Builder\Mode;
 use OpenApi\Generator;
 use OpenApi\Processors\OperationId;
+use OpenApi\Tests\Concerns\AssertsSchemaStructure;
 use OpenApi\Tests\Concerns\UsesExamples;
 use OpenApi\Utils\Pipeline;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -24,14 +27,21 @@ final class DocSnippetsTest extends OpenApiTestCase
             ->in(__DIR__ . '/../docs/snippets/')
             ->name('*.php');
 
-        foreach ($finder as $file) {
-            if (str_ends_with($file->getPathname(), '_an.php')) {
-                $other = str_replace('_an.php', '_at.php', $file->getPathname());
-                if (file_exists($other)) {
-                    $key = str_replace('_an', '', $file->getBasename('.php'));
-                    foreach ([OpenApi::VERSION_3_0_0, OpenApi::VERSION_3_1_0] as $version) {
-                        yield "{$key}-{$version}" => [['an' => $file->getPathname(), 'at' => $other], $version];
-                    }
+        foreach ($finder as $anFile) {
+            if (str_ends_with($anFile->getPathname(), '_an.php')) {
+                $atFile = str_replace('_an.php', '_at.php', $anFile->getPathname());
+                $specFile = str_replace('_an.php', '_spec.php', $anFile->getPathname());
+
+                $snippets = [
+                    'an' => $anFile->getPathname(),
+                    'at' => $atFile,
+                    'spec' => $specFile,
+                ];
+
+                $key = str_replace('_an', '', $anFile->getBasename('.php'));
+                foreach ([OpenApi::VERSION_3_0_0, OpenApi::VERSION_3_1_0, OpenApi::VERSION_3_2_0] as $version) {
+                    yield "{$key}-classic-{$version}" => [$snippets, Mode::CLASSIC, $version];
+                    yield "{$key}-hybrid-{$version}" => [$snippets, Mode::HYBRID, $version];
                 }
             }
         }
@@ -41,31 +51,40 @@ final class DocSnippetsTest extends OpenApiTestCase
      * Compare snippets and ensure they result in the same spec fragment.
      */
     #[DataProvider('snippetSets')]
-    public function testSnippets(array $filenames, string $version): void
+    public function testSnippets(array $filenames, Mode $mode, string $version): void
     {
-        $lastSpec = null;
+        $lastANSpec = null;
         foreach ($filenames as $type => $filename) {
+            if (!file_exists($filename)) {
+                continue;
+            }
+echo $filename . "\n";
             $contents = preg_replace('/(namespace [^;]+);/', "\\1\\{$type};", file_get_contents($filename));
             $namespace = basename((string) $filename, '.php');
 
-            $tmp = sys_get_temp_dir() . "/{$namespace}.php";
-            file_put_contents($tmp, $contents);
-            require_once $tmp;
+            $snippet = sys_get_temp_dir() . "/{$namespace}.php";
+            file_put_contents($snippet, $contents);
+            require_once $snippet;
 
-            $openapi = (new Generator($this->getTrackingLogger()))
+            $result = (new Builder())
+                ->setMode($type === 'spec' ? Mode::SPEC : $mode)
                 ->setVersion($version)
-                ->setTypeResolver($this->getTypeResolver())
-                 ->withProcessorPipeline(fn (Pipeline $processorPipeline): Pipeline => $processorPipeline->remove(OperationId::class))
-                ->generate([$tmp], null, false);
-            if ($lastSpec instanceof \OpenApi\Annotations\OpenApi) {
-                $this->assertSpecEquals(
-                    $openapi,
-                    $lastSpec,
-                    "Snippet: \${$filename}"
-                );
+                ->withGenerator(function (Generator $generator) use ($snippet) {
+                    $generator
+                        ->setTypeResolver($this->getTypeResolver())
+                        ->withProcessorPipeline(fn (Pipeline $processorPipeline): Pipeline => $processorPipeline->remove(OperationId::class))
+                        ->generate([$snippet], null, false);
+                })
+            ->build();
 
+            $this->assertTrue($result->isValid());
+            if ($type === 'an') {
+                $lastANSpec = $result->toArray();
+                echo 'last' . PHP_EOL;
+            } else {
+                $this->assertSpecEquals($result->toArray(), $lastANSpec);
+                echo 'compare' . PHP_EOL;
             }
-            $lastSpec = $openapi;
         }
     }
 }
