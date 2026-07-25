36ad601c1f95c68a9e1fa85aa9ca42d11ef2ce6d
DerManoMann <mano@radebatz.net>
2026-07-25 21:00:58 +1200

    move schema onto separate model

--- docs/snippets/guide/cookbook/response_examples_spec.php
+++ docs/snippets/guide/cookbook/response_examples_spec.php
@@ -4,31 +4,36 @@ namespace Openapi\Snippets\Cookbook\ResponseExamples;
 
 use OpenApi\Spec as OA;
 
+#[OA\Schema(
+    schema: 'Result',
+    type: 'object',
+    properties: [
+        new OA\Property(property: 'success', schema: new OA\Schema(type: 'boolean')),
+    ],
+)]
+class ResultModel
+{
+}
+
 class Controller
 {
-    #[OA\Schema(
-        schema: 'Result',
-        type: 'object',
-        properties: [
-            new OA\Property(property: 'success', schema: new OA\Schema(type: 'boolean')),
-        ],
-    )]
     #[OA\Response(
         response: 200,
         description: 'OK',
-        content: [new OA\MediaType(
-            mediaType: 'application/json',
-            schema: new OA\Schema(
-                oneOf: [
-                new OA\Schema(ref: '#/components/schemas/Result'),
-                new OA\Schema(type: 'boolean'),
-            ],
-                examples: [
-                new OA\Example(example: 'result', value: ['success' => true], summary: 'An result object.'),
-                new OA\Example(example: 'bool', value: false, summary: 'A boolean value.'),
-            ],
-            ),
-        )],
+        content: [
+            new OA\MediaType(
+                mediaType: 'application/json',
+                schema: new OA\Schema(
+                    oneOf: [
+                        new OA\Schema(ref: '#/components/schemas/Result'),
+                        new OA\Schema(type: 'boolean'),
+                    ],
+                    examples: [
+                        new OA\Example(example: 'result', value: ['success' => true], summary: 'An result object.'),
+                        new OA\Example(example: 'bool', value: false, summary: 'A boolean value.'),
+                    ],
+                ),
+            )],
     )]
     public function operation()
     {
